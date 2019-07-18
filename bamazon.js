06e6791676328ef06ed8b7f3ec8ec887a793e096
Dan Rosenbaum <drosenbaum5@gmail.com>
2019-07-18 10:03:46 -0400

    Added columnify. Formatted response from SQL into columns

--- bamazon.js
+++ bamazon.js
@@ -1,6 +1,12 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
+var columnify = require("columnify");
+// var columns = columnify(data, options)
+// console.log(columns)
 
+var productsList = "SELECT * FROM products ";
+
+//Create a connection to the database
 var connection = mysql.createConnection({
   host: "localhost",
 
@@ -15,8 +21,45 @@ var connection = mysql.createConnection({
   database: "bamazon_db"
 });
 
+
+
 connection.connect(function(err) {
   if (err) throw err;
   console.log("connected as id " + connection.threadId);
-  connection.end();
+  afterConnection();
+ 
 });
+
+
+//Select products from table to display in JSON format
+function afterConnection() {
+  connection.query(productsList, function(err, response) {
+    if (err) throw err;
+    var columns = columnify(response , {
+      columnSplitter: ' | ',
+      paddingChr: ' . ',
+    })
+    console.log(columns);
+    connection.end();
+  });
+}
+
+
+
+//Create User Prompt to allow item selection
+// inquirer
+// .prompt([
+//   {
+//     type: "input",
+//     name: "",
+//     message:"Please select the ID of the product",
+//     choices: [""]
+//   },
+
+//   {
+//     type: "input",
+//     name: "",
+//     message: "",
+//     choices: [""]
+//   }
+// ])
