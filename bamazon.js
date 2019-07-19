3fcb9944b1a2ff3fa9e78849eb0f58922e29cee0
Dan Rosenbaum <drosenbaum5@gmail.com>
2019-07-19 10:03:16 -0400

    Created global variables. Updated .then function to store responeses in variables.

--- bamazon.js
+++ bamazon.js
@@ -1,10 +1,13 @@
+//required packages
 var mysql = require("mysql");
 var inquirer = require("inquirer");
 var columnify = require("columnify");
-// var columns = columnify(data, options)
-// console.log(columns)
 
+//global variables
 var productsList = "SELECT * FROM products ";
+var stockCount = 0;
+var purchaseCount = 0;
+var purchaseID = 0;
 
 //Create a connection to the database
 var connection = mysql.createConnection({
@@ -22,49 +25,69 @@ var connection = mysql.createConnection({
 });
 
 
-
-connection.connect(function(err) {
+//Show connection in log if successful. Throw error if not.
+connection.connect(function (err) {
   if (err) throw err;
   console.log("connected as id " + connection.threadId);
   afterConnection();
- 
- 
+
+
 });
 
 
-//Select products from table to display in JSON format
+//Select products from table to grab JSON response and format in columns
 function afterConnection() {
-  connection.query(productsList, function(err, response) {
+  connection.query(productsList, function (err, response) {
     if (err) throw err;
-    var columns = columnify(response , {
+    var columns = columnify(response, {
       columnSplitter: ' | ',
-      paddingChr: ' . ',
+      paddingChr: '  ',
     })
-    
+    console.log(response)
     console.log(columns);
     selectItems();
     connection.end();
   });
 }
 
+
 //Function to create User Prompt to allow item selection
-function selectItems () {
+function selectItems() {
 
-inquirer
-.prompt([
-  {
-    type: "input",
-    name: "productID",
-    message:"Please select the ID of the product: ",
-    
-  },
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "productID",
+        message: "Please select the ID of the product: ",
 
-  {
-    type: "input",
-    name: "productAmount",
-    message: "Please select the amount to purchase: ",
-    
-  }
-])
+      },
 
+      {
+        type: "input",
+        name: "productAmount",
+        message: "Please select the amount to purchase: ",
+
+      }
+    ]).then(function (productChoice) {
+
+      purchaseCount = productChoice.productAmount;
+      purchaseID = productChoice.productID;
+
+      
+      console.log(productChoice.productID);
+      console.log(productChoice.productAmount);
+      updateProductAmount();
+
+    })
 }
+
+
+    function updateProductAmount () {
+      connection.query("UPDATE products SET stock_quantity = ? WHERE product name = ?",
+      // [stock_quantity -= ]
+      
+      )
+
+
+    }
